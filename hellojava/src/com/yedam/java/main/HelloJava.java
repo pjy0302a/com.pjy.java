822689a20f0aaaf8750d420dbfa2a47c187a19cf
admin <admin@YD5-22>
2021-12-28 09:05:12 +0900

    first

---
+++ hellojava/src/com/yedam/java/main/HelloJava.java
@@ -0,0 +1,13 @@
+package com.yedam.java.main;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to JAVA!");
+		System.out.println("2021-12-24");
+	
+		
+	}
+
+}
