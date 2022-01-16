1d11899d5db6dbcd4d8b3f757e46d9c725ef61bb
User <User@DESKTOP-S3LQ1IS>
2022-01-17 01:42:52 +0900

    20220117

--- project_Movie/src/com/yedam/java/app/MovieFrame.java
+++ project_Movie/src/com/yedam/java/app/MovieFrame.java
@@ -14,13 +14,15 @@ public class MovieFrame {
 	private Scanner scanner = new Scanner(System.in);
 	Login loginMain = new Login();
 	List<Login> list = movieDAO.loginAll();
-	List<Movie> mlist = movieDAO.movieList();
+	// List<Movie> mlist = movieDAO.movieList();
 	List<Movieseat> slist = movieDAO.SeatAll();
 
 	public MovieFrame() {
+
 		boolean run = true;
 		String user = "user";
 		String admin = "admin";
+
 		FIRSTMENU: while (run) {
 			// 메뉴출력
 			menuPrint();
@@ -29,10 +31,12 @@ public class MovieFrame {
 			// 각메뉴별 기능실행
 			if (menuNo == 1) {
 
-				System.out.print("아이디 입력>");
-				String id = scanner.nextLine();
-				System.out.print("비밀번호 입력>");
-				String pw = scanner.nextLine();
+//				System.out.print("아이디 입력>");
+//				String id = scanner.nextLine();
+//				System.out.print("비밀번호 입력>");
+//				String pw = scanner.nextLine();
+				String id = loginInputId();
+				String pw = loginInputPw();
 
 				for (Login loginMain : list) {
 					if (id.equals(loginMain.getLoginId()) && pw.equals(loginMain.getLoginPw())) {
@@ -47,27 +51,29 @@ public class MovieFrame {
 										int mNo = Integer.parseInt(scanner.nextLine());
 										if (mNo == 1) {
 											// 영화추가
-											Movie newMovie = new Movie();
-											System.out.print("영화제목>");
-											String name = scanner.nextLine();
-											newMovie.setMovieName(name);
-											System.out.print("영화가격>");
-											int price = Integer.parseInt(scanner.nextLine());
-											newMovie.setMoviePrice(price);
-											System.out.print("영화좌석>");
-											int seatnum = Integer.parseInt(scanner.nextLine());
-											newMovie.setMovieSeat(seatnum);
-
-											// Movieseat[] seatNum1 = new Movieseat[seatnum];
-											movieDAO.movieInsert(newMovie);
-											Movie(name, seatnum);
+											movieInsert();
+//											Movie newMovie = new Movie();
+//											System.out.print("영화제목>");
+//											String name = scanner.nextLine();
+//											newMovie.setMovieName(name);
+//											System.out.print("영화가격>");
+//											int price = Integer.parseInt(scanner.nextLine());
+//											newMovie.setMoviePrice(price);
+//											System.out.print("영화좌석>");
+//											int seatnum = Integer.parseInt(scanner.nextLine());
+//											newMovie.setMovieSeat(seatnum);
+//
+//											// Movieseat[] seatNum1 = new Movieseat[seatnum];
+//											movieDAO.movieInsert(newMovie);
+//											Movie(name, seatnum);
 
 										} else if (mNo == 2) {
 											// 영화제거
-											System.out.print("영화제목>");
-											String name = scanner.nextLine();
-											movieDAO.movieDelete(name);
-											seatedelete(name);
+											movieDelete();
+//											System.out.print("영화제목>");
+//											String name = scanner.nextLine();
+//											movieDAO.movieDelete(name);
+//											seatedelete(name);
 										} else if (mNo == 3) {
 											// 영화목록
 
@@ -97,19 +103,20 @@ public class MovieFrame {
 								int userNo = Integer.parseInt(scanner.nextLine());
 								if (userNo == 1) {
 									MovieAll();
-									System.out.print("예매 할 영화제목>");
-									String mname = scanner.nextLine();
-
-									System.out.print("예매할 티켓수>");
-									int ticket = Integer.parseInt(scanner.nextLine());
-									String loginid = loginMain.getLoginId();
-
-									SeatOne(mname, loginid);
-
-									seatChange(mname, ticket, loginid);
+									Reservation(loginMain.getLoginId());
+//									System.out.print("예매 할 영화제목>");
+//									String mname = scanner.nextLine();
+//
+//									System.out.print("예매할 티켓수>");
+//									int ticket = Integer.parseInt(scanner.nextLine());
+//									String loginid = loginMain.getLoginId();
+//
+//									SeatOne(mname, loginid);
+//
+//									seatChange(mname, ticket, loginid);
 
 								} else if (userNo == 2) {
-
+									CheckReservation(loginMain.getLoginId());
 								} else if (userNo == 3) {
 
 								} else if (userNo == 4) {
@@ -123,26 +130,11 @@ public class MovieFrame {
 				}
 
 			} else if (menuNo == 2) {
-				loginMain = new Login();
 
-				System.out.print("아이디 입력>");
-				String id = scanner.nextLine();
-				System.out.print("비밀번호 입력>");
-				String pw = scanner.nextLine();
-				System.out.print("이름 입력");
-				String name = scanner.nextLine();
-
-				for (Login logintf : list) {
-					if (id.equals(logintf.getLoginId())) {
-						System.out.println("이미있는 아이디입니다.");
-						continue FIRSTMENU;
-					}
-
+				int a = signUp();
+				if (a == 1) {
+					continue FIRSTMENU;
 				}
-				loginMain.setLoginId(id);
-				loginMain.setLoginPw(pw);
-				loginMain.setLoginName(name);
-				movieDAO.loginCreate(loginMain);
 			} else if (menuNo == 3) {
 
 			} else if (menuNo == 4) {
@@ -171,7 +163,7 @@ public class MovieFrame {
 
 	private void userPrint() {
 		System.out.println("=================================");
-		System.out.println("1.영화목록 2.금액충전 3.게시판이동 4.종료 ");
+		System.out.println("1.영화목록 2.예매확인 3.게시판이동 4.종료 ");
 		System.out.println("=================================");
 		System.out.print("메뉴선택>");
 	}
@@ -185,10 +177,6 @@ public class MovieFrame {
 
 	private void end() {
 		System.out.println("종료되었습니다.");
-	}
-
-	private void userCheck() {
-
 	}
 
 	private void MovieAll() {
@@ -205,7 +193,7 @@ public class MovieFrame {
 			if (slist.get(i).getMovieName().equals(moviename)) {
 				System.out
 						.println(" 영화명 : " + slist.get(i).getMovieName() + " 좌석 번호 : " + slist.get(i).getMovieSeatNum()
-								+ " 좌석 여부 : " + (slist.get(i).getMovieLoginId() != null ? loginId : "비어있음"));
+								+ " 좌석 여부 : " + (slist.get(i).getMovieLoginId() != null ? slist.get(i).getMovieLoginId() : "비어있음"));
 			}
 		}
 
@@ -214,7 +202,7 @@ public class MovieFrame {
 	private void seatChange(String moviename, int ticket, String loginId) {
 //		SeatOne(moviename,loginId);
 		List<Movieseat> slist = movieDAO.SeatAll();
-		
+
 		if (ticket > 0) {
 			TIKET: for (int x = 1; x <= ticket; x++) {
 				System.out.print("좌석번호 입력>");
@@ -255,8 +243,28 @@ public class MovieFrame {
 			}
 			int result = mlist.get(i).getMoviePrice() * cnt;
 
-			
 			movieDAO.movieSales(movieName, result);
+		}
+	}
+
+	private void CheckReservation(String loginId) {
+		List<Movie> mlist = movieDAO.movieList();
+		List<Movieseat> slist = movieDAO.SeatAll();
+		System.out.println(loginId);
+		for (int i = 0; i < mlist.size(); i++) {
+
+			String movieName = mlist.get(i).getMovieName();
+			
+			for (int s = 0; s < slist.size(); s++) {
+				if (slist.get(s).getMovieLoginId() != null) {
+					if (slist.get(s).getMovieLoginId().equals(loginId)) {
+						System.out.println("영화명 >" + mlist.get(i).getMovieName() + "\n" 
+											+ "예약좌석 > " + slist.get(s).getMovieSeatNum() );
+					
+					}
+					}
+			}
+
 		}
 	}
 
@@ -280,6 +288,81 @@ public class MovieFrame {
 		for (int i = 1; i <= seatNum; i++) {
 			movieDAO.seatInsert(movieName, i);
 		}
+	}
+
+	private int signUp() {
+		loginMain = new Login();
+
+		System.out.print("아이디 입력>");
+		String id = scanner.nextLine();
+		System.out.print("비밀번호 입력>");
+		String pw = scanner.nextLine();
+		System.out.print("이름 입력");
+		String name = scanner.nextLine();
+
+		for (Login logintf : list) {
+			if (id.equals(logintf.getLoginId())) {
+				System.out.println("이미있는 아이디입니다.");
+				return 1;
+			}
+
+		}
+		loginMain.setLoginId(id);
+		loginMain.setLoginPw(pw);
+		loginMain.setLoginName(name);
+		movieDAO.loginCreate(loginMain);
+		return 0;
+	}
+
+	private void Reservation(String loginId) {
+
+		System.out.print("예매 할 영화제목>");
+		String mname = scanner.nextLine();
+
+		System.out.print("예매할 티켓수>");
+		int ticket = Integer.parseInt(scanner.nextLine());
+		// String loginid = loginMain.getLoginId();
+
+		SeatOne(mname, loginId);
+
+		seatChange(mname, ticket, loginId);
+	}
+
+	public String loginInputId() {
+		System.out.print("아이디 입력>");
+		String id = scanner.nextLine();
+
+		return (id);
+	}
+
+	public String loginInputPw() {
+		System.out.print("비밀번호 입력>");
+		String pw = scanner.nextLine();
+
+		return (pw);
+	}
+
+	public void movieDelete() {
+		System.out.print("영화제목>");
+		String name = scanner.nextLine();
+		movieDAO.movieDelete(name);
+		seatedelete(name);
+	}
+
+	public void movieInsert() {
+		Movie newMovie = new Movie();
+		System.out.print("영화제목>");
+		String name = scanner.nextLine();
+		newMovie.setMovieName(name);
+		System.out.print("영화가격>");
+		int price = Integer.parseInt(scanner.nextLine());
+		newMovie.setMoviePrice(price);
+		System.out.print("영화좌석>");
+		int seatnum = Integer.parseInt(scanner.nextLine());
+		newMovie.setMovieSeat(seatnum);
+
+		movieDAO.movieInsert(newMovie);
+		Movie(name, seatnum);
 	}
 
 }
